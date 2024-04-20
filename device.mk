29989a6ea55f2d09f061772d58f4f75a830af4b4
fjrXTR <fajarslebew31@gmail.com>
2024-04-20 19:00:46 +0000

    fleur: vndk: build libbinder
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -166,7 +166,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.fleur
-    
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
@@ -476,6 +476,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libutils-v32 \
     libhidlbase-v32 \
+    libbinder-v32 \
     libui-v32
 
 # Wi-Fi
